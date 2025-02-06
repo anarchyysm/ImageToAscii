c74fbebff3ceaaaf59d47ff3abddebb8f593f151
Taz <emanuel.mag.jr@gmail.com>
2025-02-06 13:07:40 -0300

    now the program  can open images

--- Main.cpp
+++ Main.cpp
@@ -1,28 +1,33 @@
-#include <stdio.h>
-#include <opencv2/opencv.hpp>
 
-using namespace cv;
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
-int main(int argc, char** argv )
+#include <iostream>
+
+// using namespace cv;
+
+int main()
 {
-    if ( argc != 2 )
+    std::string image_path = cv::samples::findFile("meninoney.png");
+//  imread serve pra ler a imagem, e o IMREAD_COLOR significar que vai receber os bytes rbg
+//  mas existe outros metodos como IMREAD_UNCHANGED e IMREAD_GRAYSCALE
+    cv::Mat img = cv::imread(image_path, cv::IMREAD_COLOR); // mat server para armazenamento/manipulação/conversao de imagem
+							    // além de imread tb temos imwrite para escrever arquivos
+							    // como converter o meninonet.png para jpg
+							    // cv::imwrite("meninoney.jpg", img)
+    if(img.empty())
     {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
+        std::cout << "Could not read the image" << image_path << std::endl;
+        return 1;
     }
 
-    Mat image;
-    image = imread( argv[1], IMREAD_COLOR );
+    // imshow cria uma janela padrão sem precisar chamar namedWindows, mas se quiser ter mais controle sobre a janela
+    // é importante chamar named windows pra alterar propriedades da janela.
+    cv::namedWindow("Display Image", cv::WINDOW_NORMAL );
+    imshow("Display Image", img); 
 
-    if ( !image.data )
-    {
-        printf("No image data \n");
-        return -1;
-    }
-    namedWindow("Display Image", WINDOW_NORMAL );
-    imshow("Display Image", image);
-
-    waitKey(0);
+    cv::waitKey(0); // espera que uma tecla seja precionada, se não o programa fecha muito rapido
 
     return 0;
 }
