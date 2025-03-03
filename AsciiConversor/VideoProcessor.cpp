76744ce1665f65c1de5377bfb9a03c79e073e89e
Taz <emanuel.mag.jr@gmail.com>
2025-03-03 14:43:39 -0300

    added support to colors

--- AsciiConversor/VideoProcessor.cpp
+++ AsciiConversor/VideoProcessor.cpp
@@ -1,5 +1,8 @@
 #include "VideoProcessor.hpp"
 #include "colors.hpp"
+#include <cstdio>
+#include <opencv2/core/matx.hpp>
+#include <string>
  
 
 VideoProcessor::VideoProcessor(const std::string &videoPath){
@@ -37,9 +40,9 @@ std::vector<cv::Mat> VideoProcessor::getPixelizedVideo(int blockSize) {
   video.set(cv::CAP_PROP_POS_FRAMES,0);
 
   while(video.read(frame)){
-    cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
-    cv::resize(frame_gray, smallFrame, cv::Size(frame_gray.cols / blockSize, frame_gray.rows / blockSize), 0, 0, cv::INTER_LINEAR);
-    cv::resize(smallFrame, pixelizedFrame, cv::Size(frame_gray.cols, frame_gray.rows), 0, 0, cv::INTER_NEAREST);
+    //cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+    cv::resize(frame, smallFrame, cv::Size(frame.cols / blockSize, frame.rows / blockSize), 0, 0, cv::INTER_LINEAR);
+    cv::resize(smallFrame, pixelizedFrame, cv::Size(frame.cols, frame.rows), 0, 0, cv::INTER_NEAREST);
     
     pixelizedFrames.push_back(pixelizedFrame.clone());
   }  
@@ -84,7 +87,7 @@ std::vector<cv::Mat> VideoProcessor::getResizedVideo(int newWidth, double aspect
 void VideoProcessor::displayResizedVideo(const std::string &windowName){
   std::vector<cv::Mat> resizedFrames = getResizedVideo(150, 1.8, 6);
   
-  if(resizedFrames.empty()) return; 
+  if(resizedFrames.empty()) return;   
 
   for(const cv::Mat &frame : resizedFrames){
     cv::imshow(windowName, frame);
@@ -92,35 +95,57 @@ void VideoProcessor::displayResizedVideo(const std::string &windowName){
   }
 }
 
+std::string getColorCode(cv::Vec3b bgr){
+  int b, g, r;
+  b = bgr[0];
+  g = bgr[1];
+  r = bgr[2];
+  
+  std::string code = "\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m";
+
+  return code;
+}
+
 void VideoProcessor::displayASCIIArt(const std::vector<cv::Mat> &video, const std::string &asciiChars) const{
   int numChars = asciiChars.length();
-  //std::string asciiLine;
-  
+  //std::string asciiLine; 
   //cv::startWindowThread();
   //cv::namedWindow("hidden", cv::WINDOW_NORMAL);
   //cv::moveWindow("hidden", -1000, -1000);
   //cv::resizeWindow("hidden", 1, 1); 
-
+ 
   for(const cv::Mat &frame : video){
     if(frame.empty()) continue;
-    for(int y = 0; y < frame.rows; y++){
+    system("clear");
+    for(int y = 0; y < frame.rows; y++){      
       std::string asciiLine;
       for(int x = 0; x < frame.cols; ++x){
         int intensity = frame.at<uchar>(y, x);
-        std::string colorCode = hey::off; 
-        if(intensity < 85){
-          colorCode = hey::black;
-        } else if(intensity < 170){
-          colorCode = hey::grayclaro;
-        } else {
-          colorCode = hey::white;
-        }
+        cv::Vec3b pixelColor = frame.at<cv::Vec3b>(y, x);
+        //int brightness = (pixelColor[0] + pixelColor[1] + pixelColor[2]) / 3;
+        //std::string colorCode = color::off;
+        // if(intensity < 40){
+        //   colorCode = color::color_0;
+        // } else if(intensity < 100){
+        //   colorCode = color::color_237;
+        // } else if( intensity < 150 ){
+        //   colorCode = color::color_245;
+        // } else if (intensity < 200) {
+        //   colorCode = color::color_252;
+        // } else {
+        //   colorCode = color::color_255;
+        // }
         char asciiChar = asciiChars[intensity * numChars / 256];
-        asciiLine += colorCode + asciiChar + hey::off;
+        //char asciiChar = asciiChars[brightness * numChars / 256];
+        std::string colorCode = getColorCode(pixelColor);
+        asciiLine += colorCode + asciiChar + color::off;
+        //asciiLine += colorCode + asciiChar + color::off;
       }
-      std::cout << " " << asciiLine << std::endl;
+      std::cout << asciiLine << std::endl;
+      //std::cout << " " << asciiLine << std::endl;
     }
     std::cout << std::flush;
+
     // gambiarra, o certo era usar cv::waitKey(delay); mas ele só é iniciado após uma serie de eventos na tela
     // ou seja... pra ele funcionar é preciso iniciar uma tela, mesmo q seja em branco, e eu nao queria isso, pois ia poluir a tela do usuario
     // com coisas sem sentido (tela preta sem nada), então eu adicionei o delay de forma manual com this_thread sleep e chrono passando o delay
