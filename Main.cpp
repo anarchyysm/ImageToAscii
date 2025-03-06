5b9231d94a6226f97ac48d754a551219925caf54
Taz <emanuel.mag.jr@gmail.com>
2025-03-06 15:30:28 -0300

    Test bugfix

--- Main.cpp
+++ Main.cpp
@@ -1,48 +1,48 @@
 #include "AsciiConversor/ImageProcessor.hpp"
 #include "AsciiConversor/VideoProcessor.hpp"
-#include <exception>
-#include <iostream>
-#include <vector>
-
-// using namespace cv;
 
 int main() {
-  
-  // try {
-  //   ImageProcessor processor("zebra.png");
-  //
-  //   processor.displayImage("Original Image");
-  //
-  //   cv::Mat pixelizedImg = processor.getPixelizedImage(6);
-  //   cv::imshow("Pixelized Image", pixelizedImg);
-  //
-  //   cv::Mat resizedImg = processor.getResizedImage(150, 1.8);
-  //   cv::imshow("Resized Image", resizedImg);
-  //
-  //   std::string asciiChars = "  ` .'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
-  //   processor.displayASCIIArt(resizedImg, asciiChars);
-  //
-  //   cv::waitKey(0);
-  // } catch (const std::exception& ex) {
-  //   std::cerr << ex.what() << std::endl;
-  //   return 1;
-  // }
-  
+
   try {
-    VideoProcessor processor ("mickey.gif");
-    
-    std::vector<cv::Mat> video;
+    ImageProcessor processor("zebra.png");
 
-    std::string asciiChars = "  ` .'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
-    video = processor.getResizedVideo(160, 1.8, 6);
-    while(1){ 
-      processor.displayASCIIArt(video, asciiChars); 
-    }
+    //processor.displayImage("Original Image");
 
-  }
-  catch (const std::exception& ex){
+    //cv::Mat pixelizedImg = processor.getPixelizedImage(6);
+    //cv::imshow("Pixelized Image", pixelizedImg);
+
+    cv::Mat resizedImg = processor.getResizedImage(150, 1.8);
+    //cv::imshow("Resized Image", resizedImg);
+
+    std::string asciiChars = "  `.'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
+    processor.displayASCIIArt(resizedImg, asciiChars);
+
+    cv::waitKey(0);
+  } catch (const std::exception& ex) {
     std::cerr << ex.what() << std::endl;
     return 1;
   }
-  return 0; 
+
+  // try {
+  //
+  //   VideoProcessor processor("mickeycolored.gif");
+  //   // processor.displayPixelizedVideo("pixelidadooo", 3);
+  //   // processor.getResizedVideo(int newWidth, double aspectRatio, int
+  //   // blockSize)
+  //
+  //   // cv::namedWindow("dummy", cv::WINDOW_NORMAL);
+  //   // cv::waitKey(1);
+  //
+  //   std::vector<cv::Mat> video;
+  //
+  //   std::string asciiChars = "  ` .'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
+  //   video = processor.getResizedVideo(320, 1.8, 5);
+  //   while (1) {
+  //     processor.displayASCIIArt(video, asciiChars);
+  //   }
+  // } catch (const std::exception &ex) {
+  //   std::cerr << ex.what() << std::endl;
+  //   return 1;
+  // }
+  return 0;
 }
