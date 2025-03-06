657ced9c3b027558ea2ca2b8f38f8ffc592c5fa2
Taz <emanuel.mag.jr@gmail.com>
2025-03-06 19:50:14 -0300

    temporary console interface to upload files :D

--- Main.cpp
+++ Main.cpp
@@ -3,46 +3,59 @@
 
 int main() {
 
-  try {
-    ImageProcessor processor("zebra.png");
+  std::string videoPath, imagePath;
+  int opc;
 
-    //processor.displayImage("Original Image");
+  // Entrada de dados
+  std::cout << "Image to ASCII\n" << std::endl;
+  std::cout << "Options: " << std::endl;
+  std::cout << "1. Image " << std::endl;
+  std::cout << "2. Video " << std::endl;
+  std::cout << "3. Help  \n" << std::endl;
+  std::cout << "Option: ";
 
-    //cv::Mat pixelizedImg = processor.getPixelizedImage(6);
-    //cv::imshow("Pixelized Image", pixelizedImg);
+  // temp
+  std::cin >> opc;
 
-    cv::Mat resizedImg = processor.getResizedImage(150, 1.8);
-    //cv::imshow("Resized Image", resizedImg);
+  switch (opc) {
+  case 1:
+    std::cout << "image path (inside cmake-build-debug): ";
+    std::cin >> imagePath;
 
-    std::string asciiChars = "  `.'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
-    processor.displayASCIIArt(resizedImg, asciiChars);
+    try {
+      ImageProcessor processor(imagePath);
+      cv::Mat resizedImg = processor.getResizedImage(150, 1.8);
+      std::string asciiChars = "  `.'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
+      processor.displayASCIIArt(resizedImg, asciiChars);
+      cv::waitKey(0);
+    } catch (const std::exception &ex) {
+      std::cerr << ex.what() << std::endl;
+      return 1;
+    }
+    break;
+  case 2:
+    
+    std::cout << "video path (inside cmake-build-debug): ";
+    std::cin >> videoPath;
 
-    cv::waitKey(0);
-  } catch (const std::exception& ex) {
-    std::cerr << ex.what() << std::endl;
-    return 1;
+    try {
+      VideoProcessor processor(videoPath);
+      std::vector<cv::Mat> video;
+      std::string asciiChars = "  ` .'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
+      video = processor.getResizedVideo(320, 1.8, 5);
+      while (1) {
+        processor.displayASCIIArt(video, asciiChars);
+      }
+    } catch (const std::exception &ex) {
+      std::cerr << ex.what() << std::endl;
+      return 1;
+    }
+    break;
+  case 3:
+    std::cout << "Please, doesnt upload long videos, this take so much processing, wait for updates :)" << std::endl;
+    std::cout << "The image/video needs to be inside of dir cmake-build-debug/<image path>" << std::endl;
+    break;
   }
 
-  // try {
-  //
-  //   VideoProcessor processor("mickeycolored.gif");
-  //   // processor.displayPixelizedVideo("pixelidadooo", 3);
-  //   // processor.getResizedVideo(int newWidth, double aspectRatio, int
-  //   // blockSize)
-  //
-  //   // cv::namedWindow("dummy", cv::WINDOW_NORMAL);
-  //   // cv::waitKey(1);
-  //
-  //   std::vector<cv::Mat> video;
-  //
-  //   std::string asciiChars = "  ` .'`^,-~:;<>i!lI?+_1|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao23456789*#MW&8%B@";
-  //   video = processor.getResizedVideo(320, 1.8, 5);
-  //   while (1) {
-  //     processor.displayASCIIArt(video, asciiChars);
-  //   }
-  // } catch (const std::exception &ex) {
-  //   std::cerr << ex.what() << std::endl;
-  //   return 1;
-  // }
   return 0;
 }
